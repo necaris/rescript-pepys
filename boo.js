88e8989103b38afe33ea6c3e59f86462f7fedd38
Rami Chowdhury <rami.chowdhury@gmail.com>
2024-12-14 20:11:08 -0500

    remove superfluous boo.js

--- boo.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@libsql/client";
-
-const db = createClient({ url: "file:local.db" });
-const result = await db.execute("SELECT * FROM beans;");
-
-console.log(result);
